3a154934c39f4be99f71c2d237ff18186557e717
hesimin <hsm0324@163.com>
2018-06-07 13:55:02 +0800

    spring-boot.sh

--- spring-boot.sh
+++ spring-boot.sh
@@ -2,23 +2,55 @@
 
 ######################################
 # desc: spring boot start script
-# use: ./spring-boot start xxx.jar 'jvm option'
-# author: hesimin
-# version: 0.2.1
+# use:
+# ./spring-boot start xxx.jar
+# ./spring-boot -c "curl --silent --fail http://localhost:12001" start xxx.jar
+# author: hesimincn@gmail.com
+# version: 0.3
 ######################################
-
 
 echo ">>>>>>>>>>>>>>>>>  spring-boot.sh  >>>>>>>>>>>>>>>>"
 
-export JAVA_HOME=/usr/local/jdk1.8
-export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+CURR_DIR=`pwd`
+echo " current dir： ${CURR_DIR}"
 
-JAVA_OPTIONS=" -Xms300M -Xmx300M $3"
+ARGS=`getopt -o c: --long check: -n 'spring-boot.sh' -- "$@"`
+if [ $? != 0 ]; then
+    echo "Terminating..."
+    exit 1
+fi
+
+#将规范化后的命令行参数分配至位置参数（$1,$2,...)
+eval set -- "${ARGS}"
+
+while true
+do
+    case "$1" in
+        -c|--check)
+            CHECK_CMD=$2
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Options error!"
+            exit 1
+            ;;
+    esac
+done
+
+
+export JAVA_HOME=/usr/local/jdk1.8
+export PATH=${JAVA_HOME}/bin:$PATH
+export CLASSPATH=.:${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar
+
+JAVA_OPTIONS=" -Xms300M -Xmx300M "
 
 _JAR_KEYWORDS=" $2"
 if [ ! -f ${_JAR_KEYWORDS} ];then
-  echo "****** ${_JAR_KEYWORDS} ****** file not exist!"
+  echo "<<<<<<<<<<<<<<  ${_JAR_KEYWORDS} file not exist !!!!!!! " >&2
   exit 1
 fi
 #NAME=${APP_JAR%.*}
@@ -27,29 +59,72 @@ APP_NAME=$2
 
 JAVA_CMD="-jar $JAVA_OPTIONS  -XX:+HeapDumpOnOutOfMemoryError $_JAR_KEYWORDS"
 
-
-CURR_DIR=`pwd`
 echo "java_home= $JAVA_HOME"
 echo `java -version`
 echo `ps aux | grep ${_JAR_NAME} |grep java`
-echo " current dir： ${CURR_DIR}"
 echo "**** JAVA_CMD****:   $JAVA_CMD  "
 
-
-
-function check_if_process_is_running {
+function fun_get_pid {
  PID=$(ps aux | grep ${_JAR_NAME} |grep java| grep -v grep | awk '{print $2}' )
+}
+
+function fun_check_running {
+ if [ -n "$CHECK_CMD" ]; then
+     CHECK_RESULT=`eval ${CHECK_CMD}`
+     if [ -n "$CHECK_RESULT" ]; then
+         echo -e "\nCHECK_RESULT:\n ${CHECK_RESULT}"
+         return 0
+     else
+         return 1
+     fi
+ fi
+
+ CHECK_RUNNING_PID=true
+ fun_check_running_pid
+}
+
+function fun_check_running_pid {
+ fun_get_pid
  if [ "$PID" = "" ]; then
    return 1
  else
    return 0
  fi
 }
-check_if_process_is_running
+
+function fun_stop {
+    fun_get_pid
+    if [ -n "$PID" ]; then
+        kill  ${PID}
+    else
+        return 0
+    fi
+
+    echo -ne "Waiting for process to stop: $PID "
+    for i in {1..20}; do
+        if fun_check_running_pid; then
+          echo -ne ". "
+          sleep 1
+        else
+          return 0
+        fi
+    done
+
+    if fun_check_running_pid; then
+     echo -e "Cannot kill process, using kill -9 $PID "
+     kill -9 ${PID}
+     sleep 5
+    fi
+    if fun_check_running_pid; then
+       echo -e "<<<<<<<<<<<<<<  Cannot stop process  !!!!!!!!!" >&2
+       exit 1
+    fi
+}
+
 
 case "$1" in
   status)
-    if check_if_process_is_running
+    if fun_check_running
     then
       echo -e "$APP_NAME is running "
     else
@@ -57,66 +132,41 @@ case "$1" in
     fi
     ;;
   stop)
-    if ! check_if_process_is_running
+    if ! fun_check_running_pid
     then
       echo  -e "$APP_NAME  already stopped "
       exit 0
     fi
-    kill  $PID
-    echo -e "Waiting for process to stop "
-    while check_if_process_is_running; do
-        echo -ne ". "
-        sleep 1
-    done
-    echo
-    if check_if_process_is_running
-    then
-      echo -e "Cannot kill process "
-      exit 1
-    fi
-    echo  -e "$APP_NAME stop success "
+    fun_stop
     ;;
   start)
-    if check_if_process_is_running
+    if fun_check_running_pid
     then
-      #echo -e "already running: $APP_NAME  "
-      #exit 1
-      echo -ne "Waiting for process to stop: $PID "
-      kill  $PID
-      for i in {1..20}; do
-        if check_if_process_is_running; then
-          echo -ne ". "
-          sleep 1
-        fi
-      done
-      echo
-      if check_if_process_is_running; then
-         echo -e "Cannot kill process, using kill -9 $PID "
-         kill -9 $PID
-         sleep 5
-      fi
-      if check_if_process_is_running; then
-         echo -e "<<<<<<<<<<<<<<  Cannot stop process  !!!!!!!!!"
-         exit 1
-      fi
+      fun_stop
     fi
-    nohup java $JAVA_CMD >/dev/null 2>&1 &
 
-    echo -ne "Starting "
-    for i in {1..20}; do
-      if ! check_if_process_is_running; then
+    nohup java ${JAVA_CMD} >/dev/null 2>&1 &
+
+    echo
+    echo -ne "Starting. "
+    for i in {1..60}; do
+      if ! fun_check_running; then
           echo -ne ". "
           sleep 1
+      elif [[ ${CHECK_RUNNING_PID} && ${i} -lt 20 ]] ; then #pid检测的方式，需要等一段时间确认是否启动失败，此不准确
+          echo -ne ". "
+          sleep 1
+      else
+          break
       fi
     done
-    echo
 
-    sleep 10
-    if check_if_process_is_running
-     then
-       echo  -e "=========  Started  ====== $APP_NAME ========="
+    echo
+    if fun_check_running
+    then
+       echo  -e "<<<<<<<<<<<<<<  $APP_NAME start success ========="
     else
-       echo  -e "<<<<<<<<<<<<<<  Fail  !!!!!!!!!!!   $APP_NAME  !!!!!!!!!!!"
+       echo  -e "<<<<<<<<<<<<<<  $APP_NAME  start fail !!!!!!!!!!!" >&2
        exit 1
     fi
     ;;
