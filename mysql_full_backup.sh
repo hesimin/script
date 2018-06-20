610edd68d53c69e7c80c73d45ad8f76f3b4aafd8
hesimin <hsm0324@163.com>
2018-06-20 10:15:29 +0800

    mysqlfullbackup.sh

--- mysql_full_backup.sh
+++ mysql_full_backup.sh
@@ -1,21 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#  Script：                                                               +
-#      mysqlfullbackup.sh                                                 +
-#                                                                         +
-#  Description：                                                          +
-#      using mysqldump command to backup the database.                    +
-#                                                                         +
-#  Author:                                                                +
-#      WuJianJian, DBA Leader,2017.06.01                                  +
-#                                                                         +
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+######################################
+# desc: using mysqldump command to backup the database from docker.
+# use:
+# author: hesimincn@gmail.com
+# version: 0.1
+######################################
 
 # 0.初始化颜色设置
 RED_COLOR='\E[1;31m'   #红
 GREEN_COLOR='\E[1;32m' #绿
-YELOW_COLOR='\E[1;33m' #黄
+YELLOW_COLOR='\E[1;33m' #黄
 BLUE_COLOR='\E[1;34m'  #蓝
 PINK='\E[1;35m'        #粉红
 RES='\E[0m'            #关闭
@@ -28,8 +23,8 @@ function echo_green(){
     echo -e  "${GREEN_COLOR}${@:1}${RES}"
 }
 
-function echo_yelow(){
-    echo -e  "${YELOW_COLOR}${@:1}${RES}"
+function echo_yellow(){
+    echo -e  "${YELLOW_COLOR}${@:1}${RES}"
 }
 
 function echo_blue(){
@@ -43,29 +38,36 @@ function echo_pink(){
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # 1.定义变量
-mysqlcommand=/usr/local/mysql/bin/mysql
-backupcommand=/usr/local/mysql/bin/mysqldump
-binlogcommand=/usr/local/mysql/bin/mysqlbinlog
-backuphost=localhost
-backupuser=dbbackup
-backuppwd=dbbackuppwd
-backupport=3306
-backupdbs=`$mysqlcommand -h $backuphost -u $backupuser -p$backuppwd -P $backupport -e "show databases"|grep -Ev "Database|information_schema|performance_schema|mysql|test|sys"`
+mysql_docker_name=hesimin_mysql
+#
+binlogdir=/data/mysql/mysql_hesimin
 backupdir=/data/mysqlbackup
-backupbinlogdir=/data/mysqlbackup/binlogs
-logdir=/data/mysqlbackup/logs
+
+backuphost=localhost
+backupuser=root
+backuppwd=root
+backupport=13306
+
+mysqlcommand="docker exec ${mysql_docker_name} /usr/bin/mysql"
+backupcommand="docker exec ${mysql_docker_name} /usr/bin/mysqldump"
+binlogcommand="docker exec ${mysql_docker_name} /usr/bin/mysqlbinlog"
+
+backupdbs=`$mysqlcommand -h $backuphost -u $backupuser -p$backuppwd -P $backupport -e "show databases"|grep -Ev "Database|information_schema|performance_schema|mysql|sys"`
+backupbinlogdir=${backupdir}/binlogs
+logdir=${backupdir}/logs
+
 backuptime=`date +%Y%m%d`
 mkdircommand=`which mkdir`
 findcommand=`which find`
 pvcommand=`which pv`
 gzipcommand=`which gzip`
 cpcommand=`which cp`
-binlogdir=/data/mysql/mysql3306/logs
 
 
 # 2.判断目录存在与否
 [ ! -d $backupdir ] && $mkdircommand -p $backupdir
 [ ! -d $logdir ] && $mkdircommand -p $logdir
+[ ! -d $backupbinlogdir ] && $mkdircommand -p $backupbinlogdir
 
 # 3. 删除过期备份，默认只保留7天
 $findcommand $backupdir -name "*.gz" -type f -mtime +7 |xargs rm -rf
@@ -76,7 +78,7 @@ do
   echo -e "${BLUE_COLOR}${db}${RES} database backup beginning at $(date +"%Y-%m-%d %H:%M:%S")." >>$logdir/mysqlfullbackup.log
   $backupcommand -h $backuphost -u $backupuser -p$backuppwd -P $backupport $db --single-transaction --master-data=2 --flush-logs -R -E --add-drop-database --opt --set-gtid-purged=OFF | $pvcommand -q -L 10M| $gzipcommand -9 > $backupdir/$db.$backuptime.gz
   if [ $? -eq 0 ];then
-    echo -e "${BLUE_COLOR}${db}${RES} database backup ${YELOW_COLOR}successfully${RES} ending at $(date +"%Y-%m-%d %H:%M:%S")." >>$logdir/mysqlfullbackup.log
+    echo -e "${BLUE_COLOR}${db}${RES} database backup ${YELLOW_COLOR}successfully${RES} ending at $(date +"%Y-%m-%d %H:%M:%S")." >>$logdir/mysqlfullbackup.log
   else
     echo_red "$db database backup failed !!!" >>$logdir/mysqlfullbackup.log
   fi
@@ -93,7 +95,7 @@ do
   counter=$(expr $counter + 1)
   if [ $counter -eq $lognums ];then
     `$mysqlcommand -h $backuphost -u $backupuser -p$backuppwd -P $backupport -e "purge binary logs before date(now() - interval 1 day)"`
-    echo_yelow "Skip the lastest binlog file $binlogname !!!" >/dev/null
+    echo_yellow "Skip the lastest binlog file $binlogname !!!" >/dev/null
   else
     existlogfile=$backupbinlogdir/$binlogname
     if [ -e $existlogfile ];then
@@ -101,7 +103,7 @@ do
     else
      $cpcommand $binlogname $backupbinlogdir
      if [ $? -eq 0 ];then
-       echo -e "Backup mysql binlog file ${BLUE_COLOR}${binlogname}${RES} ${YELOW_COLOR}successfully${RES} at $(date +"%Y-%m-%d %H:%M:%S")." >>$logdir/mysqlfullbackup.log
+       echo -e "Backup mysql binlog file ${BLUE_COLOR}${binlogname}${RES} ${YELLOW_COLOR}successfully${RES} at $(date +"%Y-%m-%d %H:%M:%S")." >>$logdir/mysqlfullbackup.log
      else
        echo_red "Backup mysql binlog file $binlogname failed !!!" >>$logdir/mysqlfullbackup.log
      fi
